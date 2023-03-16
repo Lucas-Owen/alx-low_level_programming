96404e0939c3feb2577d31f910ed5bae1e678a64
Lucas-Owen <sanguraowens@gmail.com>
2023-03-16 07:28:07 -0700

    Comply with betty linter

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - prints 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * followed by a new line to stderr
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
