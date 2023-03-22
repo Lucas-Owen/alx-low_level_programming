d8ccedf82b3ffb706319576afcfccf64fe5f0034
Lucas-Owen <sanguraowens@gmail.com>
2023-03-21 19:21:16 -0700

    Fix output format

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,15 +18,11 @@ void print_times_table(int n)
 		{
 			if (j == 0)
 			{
-				printf("%d, ", i*j);
-			}
-			else if (j < n)
-			{
-				printf("%3d, ", i*j);
+				printf("%d", i*j);
 			}
 			else
 			{
-				printf("%3d", i*j);
+				printf(", %3d", i*j);
 			}
 		}
 		putchar('\n');
