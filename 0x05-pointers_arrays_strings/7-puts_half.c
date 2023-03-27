c4efc0917adec401dd6afd14f0185951ab8d0bd0
Lucas-Owen <sanguraowens@gmail.com>
2023-03-27 03:14:14 -0700

    Fix edge case

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,16 @@
  */
 void puts_half(char *str)
 {
-	int len = 0, i;
+	int len = 0, i, start;
 
 	while (*(str + len))
 		len++;
 
-	for (i = len / 2; i < len; i++)
+	if (len % 2 == 0)
+		start = (len - 1) / 2;
+	else
+		start = len /2;
+	for (i = start; i < len; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
 }
