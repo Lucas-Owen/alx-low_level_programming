c2337fe0e228521fe756bbc4a5217fce3792a5c0
Lucas-Owen <sanguraowens@gmail.com>
2023-03-27 03:18:05 -0700

    Fix start value

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,9 @@ void puts_half(char *str)
 		len++;
 
 	if (len % 2 == 0)
-		start = (len - 1) / 2;
+		start = len / 2;
 	else
-		start = len /2;
+		start = len / 2 + 1;
 	for (i = start; i < len; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
