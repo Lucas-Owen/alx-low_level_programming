fab4040a48ce789830d3f90552cfe53b189d3563
Lucas-Owen <sanguraowens@gmail.com>
2023-04-03 03:54:42 -0700

    Comply with betty linter

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,6 +4,7 @@
 /**
  * print_diagsums - Prints the sum of the two diagonals of a square matrix
  * @a: A pointer to an integer (Really a pointer to an array of ints)
+ * @size: Dimension of the square matrix
  * Return: void
  */
 void print_diagsums(int *a, int size)
@@ -13,7 +14,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		d1 += *(a + i * size + i);
-	       	d2 += *(a + (size - 1 - i) * size + i);
+		d2 += *(a + (size - 1 - i) * size + i);
 	}
 
 	printf("%d, %d\n", d1, d2);
