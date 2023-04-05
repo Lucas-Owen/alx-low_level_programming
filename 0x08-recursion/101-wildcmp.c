62a1279cd0b3739327a13e51b1810e077953da30
Lucas-Owen <sanguraowens@gmail.com>
2023-04-05 08:09:54 -0700

    Remove while loop

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * check_wild - Helper to compare strings in case wild card is found
+ * @s1: Pointer to first string when * is found
+ * @s2: Pointer to second string where * is found
+ * @i: Counter
+ * Return: 1 if s1 and s2 can be considered identical, 0 otherwise
+ */
+int check_wild(char *s1, char *s2, int i)
+{
+	if (*(s1 + i) == '\0')
+		return (0);
+	if (wildcmp(s1 + i, s2 + 1))
+		return (1);
+	return (check_wild(s1, s2, i + 1));
+}
+/**
  * wildcmp - Compares two strings where s2 can have a wildcard (*)
  * @s1: First string
  * @s2: Second string
@@ -8,22 +23,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	int i;
-
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '\0')
 			return (1);
-		i = 0;
-		while (*(s1 + i))
-		{
-			if (wildcmp(s1 + i, s2 + 1))
-				return (1);
-			i++;
-		}
-		return (0);
+		return (check_wild(s1, s2, 0));
 	}
 
 	if (*s1 != *s2)
