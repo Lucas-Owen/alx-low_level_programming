c95e1e5075f6889baaa544ddd224e6b69647b856
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-04-11 20:53:32 +0300

    Add failure check for malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,7 @@
  * str_concat - Concatenates two strings
  * @s1: First string
  * @s2: Second string
- * Return: Pointer to the new string
+ * Return: Pointer to the new string or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -19,6 +19,8 @@ char *str_concat(char *s1, char *s2)
 			size2++;
 
 	result = malloc((size1 + size2) * sizeof(char) + 1);
+	if (result == NULL)
+		return NULL;
 
 	result[size1 + size2] = '\0';
 	while (size2 > 0)
