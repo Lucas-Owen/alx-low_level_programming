48f961c21bfafed24def7472ced5c4bfdedb15c7
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-04-13 14:23:46 +0300

    Add 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory of nmemb elements of size bytes each
+ * and sets the memory to 0
+ * @nmemb: Number of elements
+ * @size: Size of each element
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *res;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	res = malloc(nmemb * size);
+	if (!res)
+		return (NULL);
+	for (i = 0; i < size * nmemb; i++)
+		res[i] = 0;
+	return res;
+}
