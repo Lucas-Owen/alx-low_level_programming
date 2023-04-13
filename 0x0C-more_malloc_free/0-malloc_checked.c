f7c9ef2f7569905fd022f6880b5837ff8c3df52a
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-04-13 14:15:49 +0300

    Add 0-malloc_checked.c, 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc, causes process exit (98)
+ * on failure
+ * @b: size
+ * Return: Pointer to the allocated memory space
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *res = malloc(b);
+
+	if (!res)
+		exit(98);
+	return res;
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - Concatenates first string followed by at least
+ * n bytes of the second string
+ * @s1: First string
+ * @s2: Second string
+ * Return: Pointer to the new string or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	usigned int size1 = strlen(s1);
+	usigned int size2 = strlen(s2);
+	unsigned int size = size1 + (n < size2 ? n : size2) + 1;
+	char *res = malloc(sizeof(char) * size);
+
+	if (!res)
+		return NULL;
+	strncpy(res, s1, size1);
+	strncpy(res + size1, s2, size2);
+	return res;
+}
