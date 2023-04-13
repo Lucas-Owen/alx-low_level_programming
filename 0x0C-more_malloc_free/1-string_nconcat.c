0c160b4f848c7a37d4b1a73820ae31f1bb47f464
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-04-13 22:16:46 +0300

    Add null checks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,15 +10,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size1 = strlen(s1);
-	unsigned int size2 = strlen(s2);
-	unsigned int min = (n < size2 ? n : size2);
-	unsigned int size = size1 + min;
-	char *res = malloc(sizeof(char) * size + 1);
+	unsigned int size1;
+	unsigned int size2;
+	unsigned int min;
+	unsigned int size;
+	char *res;
 
+	if (s1 == NULL)
+		size1 = 0;
+	else
+		size1 = strlen(s1);
+
+	if (s2 == NULL)
+		size2 = 0;
+	else
+		size2 = strlen(s2);
+
+	min = (n < size2 ? n : size2);
+	size = size1 + min;
+	*res = malloc(sizeof(char) * size + 1);
+	res[size] = '\0';
 	if (!res)
 		return (NULL);
-	strncpy(res, s1, size1);
-	strncpy(res + size1, s2, min);
+	if (s1 != NULL)
+		strncpy(res, s1, size1);
+	if (s2 != NULL)
+		strncpy(res + size1, s2, min);
 	return (res);
 }
