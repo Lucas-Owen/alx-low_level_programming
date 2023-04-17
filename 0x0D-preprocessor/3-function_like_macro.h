05d39145e177de46b2cbbcf283733e6987b6cfed
= <=>
2023-04-17 18:54:42 +0300

    Comply with betty

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_ABS_H
 #define FUNCTION_LIKE_MACRO_ABS_H
 
-#define ABS(x)			\
-do				\
-({				\
-	typeof (x) x_ = (x);	\
-	x_ < 0 ? -x_ : x_;	\
-})				\
-while (0)
+#define ABS(x)((x) < 0 ? -(x) : (x))
 
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,13 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_SUM_H
 #define FUNCTION_LIKE_MACRO_SUM_H
 
-#define SUM(x, y)		\
-do				\
-({				\
-	typeof (x) x_ = (x);	\
-	typeof (y) y_ = (y);	\
-	x_ + y_;		\
-})				\
-while (0)
+#define SUM(x, y)((x) + (y))
 
 #endif
