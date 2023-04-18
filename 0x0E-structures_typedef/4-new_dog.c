3d65afcee570fb34faed17786c1b6ad6582a09c6
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-04-18 06:25:41 +0300

    Fix memory leaks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 	d->name = strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
 	d->owner = strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	return (d);
 }
