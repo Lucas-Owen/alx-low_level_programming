2a942a7738f17fdb0839419f23f29cab5f417b85
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-04-19 14:25:47 +0300

    Add 100-main.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+/** main - Prints the opcodes of main
+ * @argc: Length of argv
+ * @argv: Arguments array
+ * Return: (0) on success, exits and sets status otherwise
+ */
+int main(int argc, char **argv)
+{
+	int bytes;
+	char *f = (char *) &main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (; bytes > 0; bytes--)
+	{
+		printf("%02x", (int)(*f));
+		if (bytes - 1)
+			prints(" ");
+	}
+	printf("\n");
+}
