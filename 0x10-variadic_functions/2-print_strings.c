2ff2f9169f9d2e7f736a7660f8f1f230b1e92eea
= <=>
2023-04-20 19:28:19 +0300

    Comply with betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,13 +10,13 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	int i;
+	unsigned int i;
 	char *arg;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		arg = va_arg(args, (char *));
+		arg = va_arg(args, char *);
 		if (arg)
 			printf("%s", arg);
 		else
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,70 @@
 #include <stdarg.h>
 
 /**
+ * count_specifiers - Counts the number of valid specifiers
+ * @types: list of chars
+ * Return: number of valid specifiers (c, i, f, s)
+ */
+int count_specifiers(const char * const types)
+{
+	int i = 0, n = 0;
+
+	while (types[i])
+	{
+		switch (types[i])
+		{
+			case 'c':
+			case 'i':
+			case 'f':
+			case 's':
+				n++;
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+	return (n);
+}
+
+/**
+ * print_args - Does the actual printing of the arguments and format
+ *
+ * @args: variable list of arguments
+ * @c: A format specifier
+ * Return: Whether any printing happened or not (1 or 0)
+ */
+int print_args(va_list *args, char c)
+{
+	char *arg;
+
+	switch (c)
+	{
+		case 'c':
+			printf("%c", va_arg(*args, int));
+			return (1);
+		case 'i':
+			printf("%d", va_arg(*args, int));
+			return (1);
+		case 'f':
+			printf("%f", va_arg(*args, double));
+			return (1);
+		case 's':
+			arg = va_arg(*args, char*);
+			switch ((long) arg)
+			{
+				case 0:
+					printf("(nil)");
+					break;
+				default:
+					printf("%s", arg);
+			}
+			return (1);
+		default:
+			return (0);
+	}
+}
+/**
  * print_all - Prints anythingn based on format specified
  * @format: List of types of arguments
  * Return: void
@@ -10,54 +74,18 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	char *types, *arg;
-	int i = 0, n = 0;
+	int n = 0, i = 0, printed;
 
 	va_start(args, format);
-	types = va_arg(args, (char *));
-	/* Count number of valid format specifiers*/
-	while (*types[i])
+	n = count_specifiers(format);
+	while (*(format + i))
 	{
-		switch(*types[i])
-		{
-			case 'c':
-			case 'i':
-			case 'f':
-			case 's': n++;
-				break;
-			default:
-				break;
-		}
-		i++;
-	}
-	while (*types)
-	{
-		switch(*types)
-		{
-			case 'c': printf("%c", va_arg(args, char));
-				n--;
-				break;
-			case 'i': printf("%d", va_arg(args, int));
-				n--;
-				break;
-			case 'f': printf("%f", va_arg(args, double));
-				n--;
-				break;
-			case 's': arg = va_arg(args, char*);
-				n--;
-				switch(arg)
-				{
-					case NULL: printf("(nil)");
-						break;
-					default:
-						printf("%s", arg);
-				}
-				break;
-			default: break;
-		}
-		if (n)
+		printed = print_args(&args, *(format + i));
+		if (printed)
+			n--;
+		if (n && printed)
 			printf(", ");
-		types++;
+		i++;
 	}
 	va_end(args);
 	printf("\n");
