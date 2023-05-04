c2c1e21da5f4fc1bf2e5c94e0868d473c8791578
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-05-04 08:40:43 +0300

    Add main.h, 0-binary_to_int.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+/**
+ * binary_to_uint
+ * @b: The binary number
+ * Return: The unsigned int or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = 0, num = 0, i = 0;
+
+	if (b == NULL)
+		return (num);
+	while (b[len])
+	{
+		if (b[len] == '1' || b[len] == '0')
+			len++;
+		else
+			return (num);
+	}
+	while (len > 0)
+		num &= (b[--len] - '0') << i++;
+	return (num);
+}
