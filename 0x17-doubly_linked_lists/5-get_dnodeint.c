fcbf77bbd4729db00f20ba7dafbcef388a363941
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-15 11:38:49 +0300

    Add 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_dnodeint_at_index - Get a new node at index of a doubly linked list
  * @head: Head of the doubly linked list
- * @index: Value at the new node
+ * @index: Index to search
  * Return: Address of the node if exists or NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Insert a new node at specified index of a dll
+ * @h: Head of the doubly linked list
+ * @idx: Index to insert, beginning at 0
+ * @n: Data at the new node
+ * Return: Address of the node if exists or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *curr;
+
+	if (h == NULL)
+		return (NULL);
+	if (index == 0)
+		return (add_dnodeint(h, n));
+	if (*h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	
+	for (curr = *h, --index; curr && index; --index, curr = curr->next)
+		;
+	if (!curr)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (!curr->next)
+		return (add_dnodeint_end(&curr, n));
+	curr->next->prev = new;
+	new->next = curr->next;
+	curr->next = new;
+	new->prev = curr;
+	
+	return (new);
+}
