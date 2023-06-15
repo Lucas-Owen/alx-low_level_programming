29d9ae1541918258146698a3773373daf229cd9c
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2023-06-15 10:32:13 +0300

    Add lists.h, 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elmenents of a dlistint
+ * @h: Head of the doubly linked list
+ * Return: size of the list (number of nodes)
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+	dlistint_t *temp = h;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		size++;
+		temp = temp->next;
+	}
+	printf("-> %lu elements\n", size);
+	return (size);
+}
