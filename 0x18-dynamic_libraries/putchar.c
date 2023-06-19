08b6c44811f2658cde8dd3a9355451a466aa20cc
= <=>
2023-06-19 15:26:02 +0300

    Add libdynamic.so

--- 0x18-dynamic_libraries/putchar.c
+++ 0x18-dynamic_libraries/putchar.c
@@ -1,19 +1,13 @@
-#include "main.h"
-/**
- * main - prints putchar followed by a new line
- *
- * Return: 0
- */
-int main(void)
-{
-	char pc[] = "_putchar";
-	int i = 0;
+#include <unistd.h>
 
-	while (i < 8)
-	{
-		_putchar(pc[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
