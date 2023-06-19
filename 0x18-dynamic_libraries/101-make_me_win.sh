b7535fa1f155a9df701fbafcf23341b39b89c33e
= <=>
2023-06-19 18:24:25 +0300

    Redirect output to /dev/null

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Lucas-Owen/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libatoi.so
+wget -P .. https://github.com/Lucas-Owen/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libatoi.so &> /dev/null
 export LD_PRELOAD="$PWD/../libatoi.so"
