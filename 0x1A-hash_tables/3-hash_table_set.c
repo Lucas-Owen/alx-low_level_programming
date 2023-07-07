6a6644ef4710524468254d55977735c801bb8ea0
= <=>
2023-07-07 17:13:30 +0300

    Disallow duplicate keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,29 @@ int insert_at_head(hash_node_t **head, hash_node_t *node)
 }
 
 /**
+ * check_duplicate - Checks a list for duplicate key and replaces the value
+ * @node: The head of the list
+ * @key: New key
+ * @value: New value
+ * Return: 1 on finding duplicate, 0 otherwise
+ */
+int check_duplicate(hash_node_t *node, const char *key, const char *value)
+{
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = NULL;
+			if (value)
+				node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+	return (0);
+}
+/**
  * hash_table_set - Adds an element to the hash table
  * In case of collision, adds the new node at the beginning of the list
  * @ht: Head of the hash_table
@@ -31,6 +54,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || strlen(key) == 0)
 		return (0);
+	index = key_index((unsigned char *) key, ht->size);
+	node = ht->array[index];
+	if (check_duplicate(node, key, value))
+		return (1);
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
@@ -52,7 +79,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-	index = key_index((unsigned char *) key, ht->size);
 	insert_at_head(&ht->array[index], node);
 	return (1);
 }
