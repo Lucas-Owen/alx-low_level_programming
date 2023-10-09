0569f4a8cb53f1ab18ff860770b7063aa0d77ade
Lucas Sangura <sanguraowens@gmail.com>
2023-10-09 16:45:27 +0300

    Implement Binary Search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers using
+ * the Binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: the first index where value is located or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	long int i, m, l = 0, r = size - 1;
+
+	if (!array || !size)
+		return (-1);
+	while (l <= r)
+	{
+		printf("Searching in array:");
+		for (i = l; i <= r; i++)
+			printf(" %d", array[i]);
+		printf("\n");
+		m = (l + r) / 2;
+		if (array[m] == value)
+			return (m);
+		if (array[m] < value)
+			l = m + 1;
+		else
+			r = m - 1;
+	}
+	return (-1);
+}
